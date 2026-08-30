34c0b4341b959fdf6f16751f0063688c6ec6d7fe
qtrois005-sudo <qtrois005@gmail.com>
2026-08-30 21:34:36 +0000

    Add Material Components library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Media3 / ExoPlayer
     implementation("androidx.media3:media3-exoplayer:1.4.0")
